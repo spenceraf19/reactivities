842c89b53b0afc0b39631b5c0928fc4585ba08d7
spencer.atlantic <spencer.atlantic@arzeda.com>
2026-02-19 05:19:53 -0800

    end of section 2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ActivitiesController(AppDbContext context) : BaseApiController
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+        return await context.Activities.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Activity>> GetActivityDetail(string id)
+    {
+        var activity = await context.Activities.FindAsync(id);
+        if (activity == null) return NotFound();
+        return activity;
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,34 @@
+using Domain;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await DbInitializer.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration.");
+}
+
+app.Run();
